a6fddfe494b4c0b59688210fc68f71e23f7b968d
leonard4589 <75444828+leonard4589@users.noreply.github.com>
2023-02-08 14:29:55 -0600

    .

--- phptest.php
+++ phptest.php
@@ -2,7 +2,7 @@
 
     echo "Hi <b>Brooke</b>, Why are you late???"; //have a ";" after every line
     echo "And you too Coen!!!";
-    echo $_GET; //says "Array"
+    echo "Hello" .  $_GET["name"]; //says "Array"
     print_r($_GET); //prints out contents of Array 
     
     $f = fopen("test.json", "a");
