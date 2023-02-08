121f6c0757e20a4dfd9cd5865b36a6ef995751ea
leonard4589 <75444828+leonard4589@users.noreply.github.com>
2023-02-08 14:12:20 -0600

    .

--- phptest.php
+++ phptest.php
@@ -3,7 +3,7 @@
     echo "Hi <b>Brooke</b>, Why are you late???";
     echo "And you too Coen!!!";
     echo $_GET //says "Array";
-    print_r($array);
-    var_dump($array);
+    print_r($_GET);
+    
 
 ?>
