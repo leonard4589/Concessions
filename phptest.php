1d1e808fef8236002e46409fe699eee8c149395b
leonard4589 <75444828+leonard4589@users.noreply.github.com>
2023-02-08 14:38:41 -0600

    .

--- phptest.php
+++ phptest.php
@@ -2,10 +2,10 @@
 
     echo "Hi <b>Brooke</b>, Why are you late???"; //have a ";" after every line
     echo "And you too Coen!!!<br>";
-    echo "Hello" .  $_GET["name"]; //says "Array"
+    echo "Hello" .  $_GET["name"] . "<p>"; //says "Array"
     print_r($_GET); //prints out contents of Array 
-    
-    foreach ($_GET as $id){
+    echo "<p>";
+    foreach ($_GET as $id ==> $val){
         echo $id . "==>" . $val . "<br>";
     }
 
