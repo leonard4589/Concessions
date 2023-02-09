3f3e405cc594e36b0e52b9705bbafabdb3c5cb4c
leonard4589 <75444828+leonard4589@users.noreply.github.com>
2023-02-09 13:21:21 -0600

    .

---
+++ phptest2.php
@@ -0,0 +1,20 @@
+<?php
+
+    echo "Hi <b>Brooke</b>, Why are you late???"; //have a ";" after every line
+    echo "And you too Coen!!!<br>";
+    echo "Hello" .  $_GET["name"] . "<p>"; //says "Array"
+    print_r($_GET); //prints out contents of Array 
+    echo "<p>";
+
+    foreach ($_GET as $id => $val){
+        echo $id . "==>" . $val . "<br>";
+    }
+
+    echo '<button type="button">Input!</button>'; 
+
+    $f = fopen("test.json", "a");
+    fwrite($f, "This is a file."); 
+    fclose($f);
+
+    
+?>
