f81be422744b41e5927322dd620fceacc23117a2
leonard4589 <75444828+leonard4589@users.noreply.github.com>
2023-02-23 14:40:53 -0600

    .

--- phpdata.php
+++ phpdata.php
@@ -1,11 +1,13 @@
 <?php
-
+// add information to my json file in my terminal, the a means "add" instead of completely replacing each time I hit submit
 $f = fopen("orders.json", "a");
+// writes the data to the json file
 fwrite($f, json_encode($_GET) . "\n");
 
 fclose($f);
-
+// gets the time that the submit button is pushed (epoch time)
 $_GET["Time"] = time();
+// echos what is recorded so that I know the information was saved
 echo json_encode($_GET);
 
 ?>
