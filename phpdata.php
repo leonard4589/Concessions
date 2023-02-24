1817678647ec65e84cf659d4d073df23b62fd0b5
leonard4589 <75444828+leonard4589@users.noreply.github.com>
2023-02-24 14:20:51 -0600

    .

--- phpdata.php
+++ phpdata.php
@@ -10,4 +10,24 @@ $_GET["Time"] = time();
 // echos what is recorded so that I know the information was saved
 echo json_encode($_GET);
 
+
+  // get information from the url parameters if one of the parameters in called 'name'
+  if (!empty($_GET['picoID'])){
+        
+    $_GET["time"] = time();
+    $_GET['saved'] = True;
+
+    //save info to database file
+    $dbFileName = 'pico_' . $_GET['picoID'] . '.json';
+
+    $file = fopen($dbFileName, 'a');
+    fwrite($file, json_encode($_GET)."\n");
+    fclose($file);
+    //send the information back as confimration
+    echo json_encode($_GET);
+} else {
+    echo "DATA NOT SAVED";
+    
+}
+
 ?>
